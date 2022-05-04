a74006185c19835ff220d3992c2fef330797b270
emanuelb <emanuelbenitezbaez>
2022-05-04 11:30:33 -0300

    update

--- practica-var-acumulador-contador.c
+++ practica-var-acumulador-contador.c
@@ -11,19 +11,26 @@ int main(){
     float nota ;
 float acumulador=0;
 float promedio;
+int cantNotasIngresadas =0;
 printf("\nIngrese la primer nota:");
 scanf("%f",&nota);
 acumulador=acumulador+nota;
+cantNotasIngresadas++;
 printf("\nIngrese la segunda nota:");
 scanf("%f",&nota);
 acumulador=acumulador+nota;
+cantNotasIngresadas++;
 printf("\nIngrese la tercer nota:");
 scanf("%f",&nota);
 acumulador=acumulador+nota;
+cantNotasIngresadas++;
 printf("\nIngrese la cuarta nota:");
 scanf("%f",&nota);
 acumulador=acumulador+nota;
+cantNotasIngresadas++;
 promedio= acumulador / 4;
+printf("\nLa cantidad de notas ingresadas fueron: %d",cantNotasIngresadas);
+
 printf("\nEl promedio de las notas es: %.2f",promedio);
 
 
